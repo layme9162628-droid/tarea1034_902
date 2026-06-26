5fc89680399482ac27408a1fea1781dd4567ef47
layme9162628-droid <layme.9162628@gmail.com>
2026-06-25 22:51:23 -0400

    feat: docs

---
+++ docs/_astro/hoisted.D-UWXN90.js
@@ -0,0 +1,1 @@
+import"./hoisted.BScVxmeO.js";(function(){const e=document.getElementById("site-nav");window.addEventListener("scroll",()=>{e.classList.toggle("scrolled",window.scrollY>60)},{passive:!0})})();
